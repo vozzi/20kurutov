297069e46c76434f06bdea52769d3f6cbbf80bfe
vozzi <37548527+vozzi@users.noreply.github.com>
2019-01-23 00:04:01 +0600

    Delete script.js

--- 1/script.js
+++
@@ -1,7 +0,0 @@
-for (var i = 0; i < 10; i++) {
-    setTimeout(function() {
-        console.log(i);
-    }
-};
-/*Что выведет консоль и почему?
-Я: консоль выведет ошибку « Uncaught SyntaxError: missing ) after argument list» так как не хватает закрывающей скобки после console.log(i);*/
