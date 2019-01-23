da1ac2ab8cd5c89fd523c0e8da7b2e3a1fb1b57c
vozzi <37548527+vozzi@users.noreply.github.com>
2019-01-24 04:15:17 +0600

    Update script.js

--- 2/script.js
+++ 2/script.js
@@ -1,36 +1,5 @@
-var today = new Date();
-  var day = today.getDay();
-  var daylist = ["Sunday","Monday","Tuesday","Wednesday ","Thursday","Friday","Saturday"];
-  console.log("Today is : " + daylist[day] + ".");
-  var hour = today.getHours();
-  var minute = today.getMinutes();
-  var second = today.getSeconds();
-  var prepand = (hour >= 12)? " PM ":" AM ";
-  hour = (hour >= 12)? hour - 12: hour;
-  if (hour===0 && prepand===' PM ') 
-  { 
-  if (minute===0 && second===0)
-  { 
-  hour=12;
-  prepand=' Noon';
-  } 
-  else
-  { 
-  hour=12;
-  prepand=' PM';
-  } 
-  } 
-  if (hour===0 && prepand===' AM ') 
-  { 
-  if (minute===0 && second===0)
-  { 
-  hour=12;
-  prepand=' Midnight';
-  } 
-  else
-  { 
-  hour=12;
-  prepand=' AM';
-  } 
-  } 
-document.write("Current Time : "+hour + prepand + " : " + minute + " : " + second);
+var date = new Date();
+var dateNum = date.getDay();
+var dayName = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
+document.write("Today is : " + dayName [dateNum] + '<br />');
+document.write("Current time is: " + date.getHours() + "PM :" + date.getMinutes() + " : " + date.getSeconds())
